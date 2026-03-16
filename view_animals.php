aa17ad444a0093413ed571225aad571f53142a5d
sinchana182005 <msinchana681@gmail.com>
2026-03-16 23:06:28 +0530

    Add view_animals.php to display zoo animals list

---
+++ view_animals.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>View Animals</title>
+</head>
+
+<body>
+
+<h2>Zoo Animals List</h2>
+
+<table border="1" cellpadding="10">
+
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Species</th>
+<th>Age</th>
+<th>Enclosure</th>
+</tr>
+
+<tr>
+<td>1</td>
+<td>Lion</td>
+<td>Carnivore</td>
+<td>5</td>
+<td>A1</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>Elephant</td>
+<td>Herbivore</td>
+<td>12</td>
+<td>B2</td>
+</tr>
+
+<tr>
+<td>3</td>
+<td>Tiger</td>
+<td>Carnivore</td>
+<td>6</td>
+<td>A3</td>
+</tr>
+
+</table>
+
+</body>
+</html>
