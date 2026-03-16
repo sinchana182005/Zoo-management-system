19e53dc47fe69d3c1d259ef22734ab17c3c2e688
sinchana182005 <msinchana681@gmail.com>
2026-03-16 23:04:28 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zoo Management System</title>
+<style>
+
+body{
+font-family: Arial;
+background:#d6eef2;
+text-align:center;
+}
+
+h1{
+color:#2c3e50;
+}
+
+.container{
+margin-top:50px;
+}
+
+a{
+display:block;
+width:200px;
+margin:10px auto;
+padding:10px;
+background:#3498db;
+color:white;
+text-decoration:none;
+border-radius:5px;
+}
+
+a:hover{
+background:#2980b9;
+}
+
+</style>
+</head>
+
+<body>
+
+<h1>Zoo Management System</h1>
+
+<div class="container">
+
+<a href="add_animal.php">Add Animal</a>
+
+<a href="view_animals.php">View Animals</a>
+
+</div>
+
+</body>
+</html>
